88e06512a2ed892fbfee9ccd3aa2af6eccc45078
Chris Bacon <chris.bacon@themarketinglab.co.uk>
2016-07-25 13:32:13 +0100

    created spec and address class

--- spec/CapturePlus/InteractiveRetrieve/AddressSpec.php
+++ spec/CapturePlus/InteractiveRetrieve/AddressSpec.php
@@ -55,6 +55,7 @@ UNITED KINGDOM",
             'dataLevel' => 'Premise'
         );
         $this->beConstructedWith($params);
+         new Address($params);
     }
     function it_is_initializable()
     {
@@ -63,6 +64,147 @@ UNITED KINGDOM",
 
     function it_has_an_id()
     {
-
+        $this->getId()->shouldReturn("GB|RM|A|52509479");
+    }
+    function it_has_a_domesticId()
+    {
+      $this->getDomesticId()->shouldReturn("52509479");
+    }
+    function it_has_a_language()
+    {
+      $this->getLanguage()->shouldReturn("ENG");
+    }
+    function it_has_a_languageAlternative()
+    {
+      $this->getLanguageAlternative()->shouldReturn("ENG");
+    }
+    function it_has_a_department()
+    {
+      $this->getDepartment()->shouldReturn("Sales");
+    }
+    function it_has_a_company()
+    {
+      $this->getCompany()->shouldReturn("Postcode Anywhere (Europe) Ltd");
+    }
+    function it_has_a_subBuilding()
+    {
+      $this->getSubBuilding()->shouldReturn("Test");
+    }
+    function it_has_a_buildingNumber()
+    {
+        $this->getBuildingNumber()->shouldReturn("");
+    }
+    function it_has_a_buildingName()
+    {
+      $this->getBuildingName()->shouldReturn("Waterside");
+    }
+    function it_has_a_secondaryStreet()
+    {
+      $this->getSecondaryStreet()->shouldReturn("Second Lane");
+    }
+    function it_has_a_street()
+    {
+      $this->getStreet()->shouldReturn("Basin Road");
+    }
+    function it_has_a_block()
+    {
+      $this->getBlock()->shouldReturn("ABC");
+    }
+    function it_has_a_neighbourhood()
+    {
+      $this->getNeighbourhood()->shouldReturn("Place");
+    }
+    function it_has_a_district()
+    {
+      $this->getDistrict()->shouldReturn("District place");
+    }
+    function it_has_a_city()
+    {
+      $this->getCity()->shouldReturn("Worcester");
+    }
+    function it_has_a_line1()
+    {
+      $this->getline1()->shouldReturn("Waterside");
+    }
+    function it_has_a_line2()
+    {
+      $this->getline2()->shouldReturn("Basin Road");
+    }
+    function it_has_a_line3()
+    {
+      $this->getline3()->shouldReturn("Third Address Line");
+    }
+    function it_has_a_line4()
+    {
+      $this->getline4()->shouldReturn("Fourth Address Line");
+    }
+    function it_has_a_line5()
+    {
+      $this->getline5()->shouldReturn("Fifth Address Line");
+    }
+    function it_has_a_adminAreaName()
+    {
+      $this->getAdminAreaName()->shouldReturn("Worchestershire");
+    }
+    function it_has_a_adminAreaCode()
+    {
+      $this->getAdminAreaCode()->shouldReturn("ABC");
+    }
+    function it_has_a_province()
+    {
+      $this->getProvince()->shouldReturn("Worchestershire");
+    }
+    function it_has_a_provinceCode()
+    {
+      $this->getProvinceCode()->shouldReturn("WOR");
+    }
+    function it_has_a_postalCode()
+    {
+      $this->getPostalCode()->shouldReturn("WR5 3DA");
+    }
+    function it_has_a_countryName()
+    {
+      $this->getCountryName()->shouldReturn("United Kingdom");
+    }
+    function it_has_a_countryIso2()
+    {
+      $this->getCountryIso2()->shouldReturn("GB");
+    }
+    function it_has_a_countryIso3()
+    {
+      $this->getCountryIso3()->shouldReturn("GBR");
+    }
+    function it_has_a_sortingNumber1()
+    {
+      $this->getSortingNumber1()->shouldReturn("94142");
+    }
+    function it_has_a_sortingNumber2()
+    {
+      $this->getSortingNumber2()->shouldReturn("45678");
+    }
+    function it_has_a_barcode()
+    {
+      $this->getBarcode()->shouldReturn("(WR53DA1PX)");
+    }
+    function it_has_a_POBoxNumber()
+    {
+      $this->getPOBoxNumber()->shouldReturn("34567");
+    }
+    function it_has_a_label()
+    {
+      $this->getLabel()->shouldReturn("Postcode Anywhere (Europe) Ltd
+Waterside
+Basin Road
+WORCESTER
+WR5 3DA
+UNITED KINGDOM");
+    }
+    function it_has_a_type()
+    {
+      $this->getType()->shouldReturn("Commercial");
+    }
+    function it_has_a_dataLevel()
+    {
+      $this->getDataLevel()->shouldReturn("Premise");
     }
 }
--- src/CapturePlus/InteractiveRetrieve/Address.php
+++ src/CapturePlus/InteractiveRetrieve/Address.php
@@ -4,8 +4,199 @@ namespace TheMarketingLab\PCA\CapturePlus\InteractiveRetrieve;
 
 class Address
 {
-    public function __construct($argument1)
+    private $id;
+    public function __construct($options)
     {
-        // TODO: write logic here
+        $this->id = $options["id"];
+        $this->domesticId = $options["domesticId"];
+        $this->language = $options["language"];
+        $this->languageAlternatives = $options["languageAlternatives"];
+        $this->department = $options["department"];
+        $this->company = $options["company"];
+        $this->subBuilding = $options["subBuilding"];
+        $this->buildingNumber = $options["buildingNumber"];
+        $this->buildingName = $options["buildingName"];
+        $this->secondaryStreet = $options["secondaryStreet"];
+        $this->street = $options["street"];
+        $this->block = $options["block"];
+        $this->neighbourhood = $options["neighbourhood"];
+        $this->district = $options["district"];
+        $this->city = $options["city"];
+        $this->line1 = $options["line1"];
+        $this->line2 = $options["line2"];
+        $this->line3 = $options["line3"];
+        $this->line4 = $options["line4"];
+        $this->line5 = $options["line5"];
+        $this->adminAreaName = $options["adminAreaName"];
+        $this->adminAreaCode = $options["adminAreaCode"];
+        $this->province = $options["province"];
+        $this->provinceCode = $options["provinceCode"];
+        $this->postalCode = $options["postalCode"];
+        $this->countryName = $options["countryName"];
+        $this->countryIso2 = $options["countryIso2"];
+        $this->countryIso3 = $options["countryIso3"];
+        $this->sortingNumber1 = $options["sortingNumber1"];
+        $this->sortingNumber2 = $options["sortingNumber2"];
+        $this->barcode = $options["barcode"];
+        $this->POBoxNumber = $options["POBoxNumber"];
+        $this->label = $options["label"];
+        $this->type = $options["type"];
+        $this->dataLevel = $options["dataLevel"];
+    }
+
+    public function getId()
+    {
+      return $this->id;
+    }
+
+    public function getDomesticId()
+    {
+        return $this->domesticId;
+    }
+
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    public function getLanguageAlternative()
+    {
+        return $this->languageAlternatives;
+    }
+
+    public function getDepartment()
+    {
+        return $this->department;
+    }
+
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    public function getSubBuilding()
+    {
+        return $this->subBuilding;
+    }
+
+    public function getBuildingNumber()
+    {
+        return $this->buildingNumber;
+    }
+
+    public function getBuildingName()
+    {
+        return $this->buildingName;
+    }
+    public function getSecondaryStreet()
+    {
+      return $this->secondaryStreet;
+    }
+
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    public function getBlock()
+    {
+        return $this->block;
+    }
+
+    public function getNeighbourhood()
+    {
+        return $this->neighbourhood;
+    }
+
+    public function getDistrict()
+    {
+        return $this->district;
+    }
+
+    public function getCity()
+    {
+        return $this->city;
+    }
+    public function getline1()
+    {
+        return $this->line1;
+    }
+    public function getline2()
+    {
+        return $this->line2;
+    }
+    public function getline3()
+    {
+        return $this->line3;
+    }
+    public function getline4()
+    {
+        return $this->line4;
+    }
+    public function getline5()
+    {
+        return $this->line5;
+    }
+    public function getAdminAreaName()
+    {
+        return $this->adminAreaName;
+    }
+    public function getAdminAreaCode()
+    {
+        return $this->adminAreaCode;
+    }
+    public function getProvince()
+    {
+        return $this->province;
+    }
+    public function getProvinceCode()
+    {
+        return $this->provinceCode;
+    }
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+    public function getCountryName()
+    {
+        return $this->countryName;
+    }
+    public function getCountryIso2()
+    {
+        return $this->countryIso2;
+    }
+    public function getCountryIso3()
+    {
+        return $this->countryIso3;
+    }
+    public function getSortingNumber1()
+    {
+        return $this->sortingNumber1;
+    }
+    public function getSortingNumber2()
+    {
+        return $this->sortingNumber2;
+    }
+    public function getBarcode()
+    {
+        return $this->barcode;
+    }
+    public function getPOBoxNumber()
+    {
+        return $this->POBoxNumber;
+    }
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function getDataLevel()
+    {
+        return $this->dataLevel;
     }
 }
